1720d2b188c63b84e2794735e3d97d2032144424
Nabil B. <nbenkerdag@laas.fr>
2026-07-28 14:53:34 +0200

    Edit of the initialisation for the car, send policy one by one

--- src/local/odrl_eval/config.py
+++ src/local/odrl_eval/config.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import json
-import os
+from pathlib import Path
+
+import requests
 
 
 @dataclass
@@ -14,46 +16,68 @@ class Service:
         return f"http://{self.ip}:{self.port}{self.endpoint}"
 
 
-@dataclass
 class BundleConfig:
-    port: int = 8000
-    cloud_ip: str = "[cloud-server-ip]"
-    edge_ip: str = "[edge-server-ip]"
-    policies_path: str = "./policies"
+    def __init__(self):
+        self.port: int = 8000
+        self.cloud_ip: str = "[cloud-server-ip]"
+        self.edge_ip: str = "[edge-server-ip]"
+        self.policies_path = Path("./policies")
 
-    endpoints: dict = field(
-        default_factory=lambda: {
+        self.endpoints = {
             "decision": "/decision",
             "identification": "/identification",
             "navigation": "/navigation",
         }
-    )
 
-    bundle: dict[str, Service] = field(default_factory=dict)
+        self.bundle_decision: dict[str, Service] = {}
 
-    def import_odrl(self) -> dict:
+        self.validated_odrl = self.initialisation()
+        self.odrl_to_config(self.validated_odrl)
+
+    def initialisation(self) -> dict:
         graph = []
-
-        for filename in os.listdir(self.policies_path):
-            if not filename.endswith(".json") or filename == "template-policy.json":
+        for policy_file in self.policies_path.glob("*.json"):
+            if policy_file.name == "template-policy.json":
                 continue
 
-            filepath = os.path.join(self.policies_path, filename)
-
-            with open(filepath, "r", encoding="utf-8") as f:
-                graph.append(json.load(f))
+            policy = json.loads(policy_file.read_text(encoding="utf-8"))
+            if self.ask_orchestrator(policy):
+                graph.append(policy)
 
         return {
             "@context": "http://www.w3.org/ns/odrl.jsonld",
             "@graph": graph,
         }
 
-    @classmethod
-    def from_odrl(cls):
-        config = cls()
-        odrl = config.import_odrl()
-        config.odrl_to_config(odrl)
-        return config
+    def ask_orchestrator(self, policy) -> bool:
+        reply = requests.post("ORCHESTRATOR_DOMAIN", json=policy)
+
+        try:
+            reply.raise_for_status()
+        except requests.HTTPError as e:
+            raise RuntimeError(
+                f"There was an error when contacting the Orchestrator: {e}"
+            ) from e
+
+        result = reply.json()
+        success = result.get("result")
+        if success is True:
+            return True
+        elif success is False:
+            reason = result.get("reason")
+            constraints = [
+                f"{c['leftOperand']} {c['operator']} {c['rightOperand']}"
+                for c in reason
+            ]
+
+            print(
+                "There is no service available that is compliant with these constraints: "
+                + ", ".join(constraints)
+            )
+
+            return False
+        else:
+            raise RuntimeError(f"Unexpected API response: {result}")
 
     def odrl_to_config(self, odrl: dict):
         for policy in odrl["@graph"]:
@@ -70,11 +94,11 @@ class BundleConfig:
 
             endpoint = self.endpoints[service_name]
 
-            self.bundle[service_name] = Service(
+            self.bundle_decision[service_name] = Service(
                 ip=ip,
                 port=self.port,
                 endpoint=endpoint,
             )
 
 
-bundle_config = BundleConfig.from_odrl().bundle
+bundle_config = BundleConfig()
