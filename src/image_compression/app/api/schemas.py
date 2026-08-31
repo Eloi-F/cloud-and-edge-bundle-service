7601e7d51eb192bebf4763976851de74ae9a41f8
Eloi-F <eloi.favard@insa-toulouse.fr>
2026-08-31 08:53:41 +0200

    Remove service related api folders

--- src/image_compression/app/api/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class ResizeRequest(BaseModel):
-    bundle_id: str
-    front: float
-    image: str
-    metadata: dict
-    state: bool
--- src/navigation/app/api/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class TrajectoryRequest(BaseModel):
-    start_address: str
-    destination_address: str
-    metadata: dict
-    bundle_id: str
